b144748dd4990b14fc14f89049f9e0c36635da6c
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 20:02:41 -0300

    set task model

---
+++ api/models/entities/task_entity.py
@@ -0,0 +1,23 @@
+from datetime import datetime, date
+from typing import Text
+from sqlalchemy import Boolean, Date, DateTime, ForeignKey, Integer, String, func, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from api.configs.db.database import Base
+from api.models.entities.user_entity import UserEntity
+
+class TaskEntity(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    is_done: Mapped[bool] = mapped_column(Boolean, default=False)
+    due_date: Mapped[date | None] = mapped_column(Date, nullable=True)
+    priority: Mapped[int | None] = mapped_column(Integer, nullable=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    owner: Mapped["UserEntity"] = relationship("UserEntity", back_populates="tasks")
