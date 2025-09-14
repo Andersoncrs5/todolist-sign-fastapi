480ee78ba2a0a1218013575b976f852e3d36483e
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 20:08:21 -0300

    new methods

--- api/repositories/base/base_user_repository.py
+++ api/repositories/base/base_user_repository.py
@@ -1,7 +1,35 @@
 from abc import ABC, abstractmethod
+from api.models.entities.user_entity import UserEntity
 
 class BaseUserRepository(ABC):
     @abstractmethod
     def exists_by_email(self, email: str) -> bool:
         pass
 
+    @abstractmethod
+    def get_by_email(self, email: str) -> (UserEntity | None):
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id: int) -> (UserEntity | None):
+        pass
+
+    @abstractmethod
+    def create(self, user: UserEntity) -> UserEntity:
+        pass
+
+    @abstractmethod
+    def delete(self, user: UserEntity):
+        pass
+    
+    @abstractmethod
+    def update(self, user: UserEntity) -> UserEntity:
+        pass
+
+    @abstractmethod
+    def refresh_token(self, refresh_token: str, user: UserEntity) -> UserEntity:
+        pass
+
+    @abstractmethod
+    def save(self, user: UserEntity) -> UserEntity:
+        pass
