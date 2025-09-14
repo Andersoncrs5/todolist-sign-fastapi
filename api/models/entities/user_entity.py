0b609190ca7ff3268c29968ae7d37bb9dfef0f25
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 20:03:00 -0300

    add table user entity

---
+++ api/models/entities/user_entity.py
@@ -0,0 +1,19 @@
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+
+from api.configs.db.database import Base
+from api.models.entities.task_entity import TaskEntity
+
+class UserEntity(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    refresh_token: Mapped[str | None] = mapped_column(String(), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    tasks: Mapped[list["TaskEntity"]] = relationship("TaskEntity", back_populates="owner")
