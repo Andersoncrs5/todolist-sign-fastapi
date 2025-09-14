02712cf4951780240b7d41c3d4e4b4ab21b2dbfa
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 20:08:09 -0300

    set user repository base

---
+++ api/repositories/base/base_task_repository.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from api.models.entities.task_entity import TaskEntity
+from utils.filters.task_filter import TaskFilter
+
+class BaseTaskRepository(ABC):
+
+    @abstractmethod
+    def get_by_id(self, id: int) -> TaskEntity | None:
+        pass
+    
+    @abstractmethod
+    def get_all_user_id_filtered(self, user_id: int, filters: TaskFilter) -> list[TaskEntity]: 
+        pass
+
+    @abstractmethod
+    def delete(self, task: TaskEntity):
+        pass
+
+    @abstractmethod
+    def save(self, task: TaskEntity) -> TaskEntity:
+        pass
+
+    @abstractmethod
+    def create(self, task: TaskEntity) -> TaskEntity:
+        pass
