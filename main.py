9338a26cee0d63b31a3448d7f201c97b199f1fc3
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 22:08:22 -0300

    set logs and router auth

--- main.py
+++ main.py
@@ -2,3 +2,28 @@
 # source venv/bin/activate
 # pip install -r requirements.txt
 # uvicorn main:app --reload
+
+import logging
+from fastapi import FastAPI
+from api.controllers import auth_controller
+from api.configs.db.database import create_tables
+from contextlib import asynccontextmanager
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Starting up the application...")
+    
+    create_tables()
+    yield
+    logger.info("Shutting down the application...")
+
+app.include_router(auth_controller.router)
+
