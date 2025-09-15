e61d78de263f18bb7651ea13acebdadff49b53b7
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-15 18:56:47 -0300

    new methods

--- api/services/base/base_task_service.py
+++ api/services/base/base_task_service.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 from api.models.entities.task_entity import TaskEntity
+from api.models.entities.user_entity import UserEntity
+from api.utils.filters.task_filter import TaskFilter
+from typing import List
+from api.models.schemas.task_schemas import UpdateTaskDTO, CreateTaskDTO
 
-class TaskService(ABC):
+class BaseTaskService(ABC):
+
     @abstractmethod
     def get_by_id(self, id: int) -> (TaskEntity | None):
         pass
@@ -9,3 +14,20 @@ class TaskService(ABC):
     @abstractmethod
     def delete(self, task: TaskEntity):
         pass
+
+    @abstractmethod
+    def create(self, user: UserEntity, dto: CreateTaskDTO) -> TaskEntity:
+        pass
+
+    @abstractmethod
+    def update(self, task: TaskEntity, dto: UpdateTaskDTO) -> TaskEntity:
+        pass
+
+    @abstractmethod
+    def get_all_user_id_filtered(self, user_id: int, filters: TaskFilter) -> List[TaskEntity]: 
+        pass
+
+    @abstractmethod
+    def change_status_done(self, task: TaskEntity) -> TaskEntity:
+        pass
+    
