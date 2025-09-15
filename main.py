1a9c2554ea9fe032045967a619d63e402acaf403
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-15 18:52:29 -0300

    set routes, details about api, config logs and another thing

--- main.py
+++ main.py
@@ -5,17 +5,16 @@
 
 import logging
 from fastapi import FastAPI
-from api.controllers import auth_controller
+from api.controllers import auth_controller, user_controller, task_controller
 from api.configs.db.database import create_tables
 from contextlib import asynccontextmanager
+from typing import Final
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s"
 )
-logger = logging.getLogger(__name__)
-
-app = FastAPI()
+logger: Final[logging.Logger] = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,5 +24,12 @@ async def lifespan(app: FastAPI):
     yield
     logger.info("Shutting down the application...")
 
-app.include_router(auth_controller.router)
+app: Final[FastAPI] = FastAPI(
+    lifespan=lifespan, 
+    title="To do list with system auth with jwt", 
+    version="1.0.0"
+    )
 
+app.include_router(task_controller.router)
+app.include_router(auth_controller.router)
+app.include_router(user_controller.router)
