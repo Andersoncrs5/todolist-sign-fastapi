78b1173f88a7adf0beff189b084673d9a8deb868
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 22:12:27 -0300

    set response body to response http

---
+++ api/utils/res/response_body.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import TypeVar, Generic
+from datetime import datetime
+
+T = TypeVar('T')
+
+class ResponseBody(BaseModel, Generic[T]):
+    code: int
+    message: str
+    body: T
+    status: bool
+    datetime: datetime
